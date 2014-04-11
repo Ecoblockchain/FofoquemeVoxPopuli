a861857f18ddad9b7a87baa1cdae4a22732fc517
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-04-11 02:36:12 -0700

    adds IOIO pin control. simple test sweeps

--- Android/src/me/fofoque/voxpopuli/MainActivity.java
+++ Android/src/me/fofoque/voxpopuli/MainActivity.java
@@ -1,6 +1,8 @@
 package me.fofoque.voxpopuli;
 
+import ioio.lib.api.DigitalInput;
 import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.PwmOutput;
 import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.BaseIOIOLooper;
 import ioio.lib.util.IOIOLooper;
@@ -17,7 +19,6 @@ import android.widget.ToggleButton;
  * HelloIOIOPower example.
  */
 public class MainActivity extends IOIOActivity {
-	private ToggleButton button_;
 
 	/**
 	 * Called when the activity is first created. Here we normally initialize
@@ -27,7 +28,6 @@ public class MainActivity extends IOIOActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		button_ = (ToggleButton) findViewById(R.id.button);
 	}
 
 	/**
@@ -38,8 +38,25 @@ public class MainActivity extends IOIOActivity {
 	 * be called repetitively until the IOIO gets disconnected.
 	 */
 	class Looper extends BaseIOIOLooper {
-		/** The on-board LED. */
-		private DigitalOutput led_;
+		private static final int PANSWITCH0 = 35;
+		private static final int PANSWITCH1 = 39;
+		private static final int TILTSWITCH0 = 43;
+		private static final int TILTSWITCH1 = 46; // soon
+
+		private static final int PANMOTOR0 = 12;
+		private static final int PANMOTOR1 = 14;
+		private static final int TILTMOTOR0 = 3;
+		private static final int TILTMOTOR1 = 6;
+
+		private static final int PWMFREQ = 2000;
+
+		// these should be in a class...
+		private DigitalInput panSwitch0, panSwitch1, tiltSwitch0, tiltSwitch1;
+		private PwmOutput panMotor0, panMotor1, tiltMotor0, tiltMotor1;
+
+		// state
+		private int panDirection, tiltDirection;
+		private float currentPanDutyCycle, currentTiltDutyCycle;
 
 		/**
 		 * Called every time a connection with IOIO has been established.
@@ -52,7 +69,18 @@ public class MainActivity extends IOIOActivity {
 		 */
 		@Override
 		protected void setup() throws ConnectionLostException {
-			led_ = ioio_.openDigitalOutput(0, true);
+			panSwitch0 = ioio_.openDigitalInput(PANSWITCH0, DigitalInput.Spec.Mode.PULL_UP);
+			panSwitch1 = ioio_.openDigitalInput(PANSWITCH1, DigitalInput.Spec.Mode.PULL_UP);
+			tiltSwitch0 = ioio_.openDigitalInput(TILTSWITCH0, DigitalInput.Spec.Mode.PULL_UP);
+			tiltSwitch1 = ioio_.openDigitalInput(TILTSWITCH1, DigitalInput.Spec.Mode.PULL_UP);
+
+			panMotor0 = ioio_.openPwmOutput(PANMOTOR0, PWMFREQ);
+			panMotor1 = ioio_.openPwmOutput(PANMOTOR1, PWMFREQ);
+			tiltMotor0 = ioio_.openPwmOutput(TILTMOTOR0, PWMFREQ);
+			tiltMotor1 = ioio_.openPwmOutput(TILTMOTOR1, PWMFREQ);
+
+			panDirection = tiltDirection = 1;
+			currentPanDutyCycle = currentTiltDutyCycle = 0.0f;
 		}
 
 		/**
@@ -65,11 +93,37 @@ public class MainActivity extends IOIOActivity {
 		 */
 		@Override
 		public void loop() throws ConnectionLostException {
-			led_.write(!button_.isChecked());
 			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
+				if(!panSwitch0.read()){
+					panDirection = -1;
+					currentPanDutyCycle = 0.0f;
+				}
+				if(!panSwitch1.read()){
+					panDirection = 1;
+					currentPanDutyCycle = 0.0f;
+				}
+
+				if(!tiltSwitch0.read()){
+					tiltDirection = -1;
+					currentTiltDutyCycle = 0.0f;
+				}
+				if(!tiltSwitch1.read()){
+					tiltDirection = 1;
+					currentTiltDutyCycle = 0.0f;
+				}
+
+				panMotor0.setDutyCycle((panDirection>0)?currentPanDutyCycle:0.0f);
+				panMotor1.setDutyCycle((panDirection>0)?0.0f:currentPanDutyCycle);
+
+				tiltMotor0.setDutyCycle((tiltDirection>0)?currentTiltDutyCycle:0.0f);
+				tiltMotor1.setDutyCycle((tiltDirection>0)?0.0f:currentTiltDutyCycle);
+
+				currentPanDutyCycle += (currentPanDutyCycle<0.5)?0.005f:0.0f;
+				currentTiltDutyCycle += (currentTiltDutyCycle<0.5)?0.005f:0.0f;
+
+				Thread.sleep(10);
 			}
+			catch (InterruptedException e) { }
 		}
 	}
 
