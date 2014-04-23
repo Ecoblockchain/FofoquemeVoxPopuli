55fe0a9ff1d47bcd643ddb91a3aaad6088f90771
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-04-22 21:54:51 -0700

    slows down motors, makes sure the switches stay hit when they hit, by measuring them before  stopping the motor

--- Arduino/RandomMoveTest/VoxMotor.cpp
+++ Arduino/RandomMoveTest/VoxMotor.cpp
@@ -1,7 +1,9 @@
 #include "Arduino.h"
 #include "VoxMotor.h"
 
-#define PWM_MAX_DUTY 0.8
+#define PWM_CALIBRATE_DUTY 0.5
+#define PWM_MAX_DUTY 0.6
+
 
 VoxMotor::VoxMotor(){
   pin[0] = pin[1] = 0;
@@ -25,26 +27,27 @@ void VoxMotor::setup(int motor0, int motor1, int switch0, int switch1){
   // make sure switches are not pressed initially
   if(!(digitalRead(limit[0]) && digitalRead(limit[1]))){
     analogWrite(pin[0], 255);
-    analogWrite(pin[1], 85);
-    delay(200);
+    analogWrite(pin[1], (1.0-PWM_CALIBRATE_DUTY)*255.0);
+    delay(300);
     analogWrite(pin[0], 255);
     analogWrite(pin[1], 255);
+    delay(500);
   }
+
   if(!(digitalRead(limit[0]) && digitalRead(limit[1]))){
-    analogWrite(pin[0], 85);
+    analogWrite(pin[0], (1.0-PWM_CALIBRATE_DUTY)*255.0);
     analogWrite(pin[1], 255);
-    delay(200);
+    delay(300);
     analogWrite(pin[0], 255);
     analogWrite(pin[1], 255);
+    delay(500);
   }
 
   while(digitalRead(limit[0]) && digitalRead(limit[1])){
     // pwm motor0
-    analogWrite(pin[0], 85);
+    analogWrite(pin[0], (1.0-PWM_CALIBRATE_DUTY)*255.0);
     analogWrite(pin[1], 255);
   }
-  // some switch hit: stop
-  analogWrite(pin[0], 255);
 
   // if limit[1] was hit, swicth limits
   if(digitalRead(limit[0])){
@@ -55,7 +58,7 @@ void VoxMotor::setup(int motor0, int motor1, int switch0, int switch1){
   // unpress switch
   while(!(digitalRead(limit[0]) && digitalRead(limit[1]))){
     analogWrite(pin[0], 255);
-    analogWrite(pin[1], 85);
+    analogWrite(pin[1], (1.0-PWM_CALIBRATE_DUTY)*255.0);
   }
 
   changeStateMillis = millis() + 1000;
