b78ae600fea5289bfdc7e4e09bdce308fdd8894b
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-05-02 16:48:26 -0700

    changes to work with USB Library 2.0

--- Arduino/ADKTest/ADKTest.ino
+++ Arduino/ADKTest/ADKTest.ino
@@ -1,25 +1,36 @@
-#include <Max3421e.h>
-#include <Usb.h>
-#include <AndroidAccessory.h>
-#define  LED_PIN  13
+#include <adk.h>
 
-AndroidAccessory acc("Arduino", "ADK", "Description", "1.0", "http://fofoque.me", "0000000012345678");
+#define LED 13
+
+USB Usb;
+ADK adk(&Usb, "Arduino", "ADK", "Description", "1.0", "http://fofoque.me", "0000000012345678");
 
 void setup() {
-  // set communiation speed
   Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
-  acc.powerOn();
+  if (Usb.Init() == -1) {
+    Serial.print("\r\nOSCOKIRQ failed to assert");
+    while (1); // halt
+  }
+  pinMode(LED, OUTPUT);
+  Serial.print("\r\nArduino ADK Test Started");
 }
 
 void loop() {
-  byte msg[0];
-  if (acc.isConnected()) {
-    int len = acc.read(msg, sizeof(msg), 1);
-    if (len > 0) {
-      digitalWrite(LED_PIN, msg[0]?HIGH:LOW);
+  Usb.Task();
+
+  if (adk.isReady()) {
+    uint8_t msg[1];
+    uint16_t len = sizeof(msg);
+    uint8_t rcode = adk.RcvData(&len, msg);
+    if (rcode && rcode != hrNAK) {
+      Serial.print(F("\r\nData rcv: "));
+      Serial.print(rcode, HEX);
+    }
+    else if (len > 0) {
+      Serial.print(F("\r\nData Packet: "));
+      Serial.print(msg[0]);
+      digitalWrite(LED, msg[0] ? HIGH : LOW);
     }
   }
 }
-
 
