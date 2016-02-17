598c0fe8f11408464194f02f622856fe30b87146
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-02-17 22:37:20 +0000

    removes OSC and communication to server. There is no server\!

--- Android/VoxPopuliSingle/src/me/fofoque/voxpopuli/VoxPopuliActivity.java
+++ Android/VoxPopuliSingle/src/me/fofoque/voxpopuli/VoxPopuliActivity.java
@@ -3,17 +3,11 @@ package me.fofoque.voxpopuli;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Locale;
 import java.util.Queue;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.UUID;
 
 import android.app.Activity;
@@ -34,17 +28,9 @@ import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.Engine;
 import android.telephony.SmsMessage;
 
-import com.illposed.osc.OSCMessage;
-import com.illposed.osc.OSCListener;
-import com.illposed.osc.OSCPortIn;
-import com.illposed.osc.OSCPortOut;
-
 public class VoxPopuliActivity extends Activity implements TextToSpeech.OnInitListener {
 	// TAG is used to debug in Android logcat console
 	private static final String TAG = "VoxPopTag ";
-	private static final String VOX_SERVER_ADDRESS = "200.0.0.101";
-	private static final int OSC_OUT_PORT = 8888;
-	private static final int OSC_IN_PORT = 8989;
 	private static final UUID SERIAL_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 	private static final String BLUETOOTH_ADDRESS = "00:14:03:06:19:99";
 
@@ -57,30 +43,8 @@ public class VoxPopuliActivity extends Activity implements TextToSpeech.OnInitLi
 
 	private TextToSpeech mTTS = null;
 	private SMSReceiver mSMS = null;
-	
-	private OSCPortIn mOscIn = null;
-	private OSCPortOut mOscOut = null;
-	private String oscOutAdressString = "";
+
 	private String smsMessageText = "";
-
-	OSCListener mOscListener = new OSCListener() {
-		public void acceptMessage(Date time, OSCMessage message) {
-			// read: msg, pan, tilt from osc
-			String msg = (String)(message.getArguments().get(0));
-			int pan = ((Integer)(message.getArguments().get(1))).intValue();
-			int tilt = ((Integer)(message.getArguments().get(2))).intValue();
-			int delay = ((Integer)(message.getArguments().get(3))).intValue();
-
-			Log.d(TAG, "OSC got : "+msg+" "+pan+" "+tilt+" "+delay+" from RPI");
-			msgQueue.offer(new MotorMessage(msg, pan, tilt));
-
-			new Timer().schedule(new TimerTask() {
-			    @Override
-			    public void run() {
-					checkQueues();
-			    }}, delay);
-		}
-	};
 
 	private class MotorMessage {
 		public String msg;
@@ -95,8 +59,7 @@ public class VoxPopuliActivity extends Activity implements TextToSpeech.OnInitLi
 	private Queue<MotorMessage> msgQueue = null;
 
 	// listen for intent sent by broadcast of SMS signal
-	// if it gets a new SMS
-	//  clean it up a little bit and send to server
+	// if it gets a new SMS, clean it up a little bit and put on queue
 	public class SMSReceiver extends BroadcastReceiver {
 		@Override
 		public void onReceive(Context context, Intent intent) {
@@ -121,20 +84,12 @@ public class VoxPopuliActivity extends Activity implements TextToSpeech.OnInitLi
 						smsMessageText = smsMessageText.replaceAll("[@#]?", "");
 						smsMessageText = smsMessageText.replaceAll("[():]+", "");
 
-						// send to server
-						Thread thread = new Thread(new Runnable(){
-						    @Override
-						    public void run() {
-								try{
-									OSCMessage oscMsg = new OSCMessage("/ffqmesms");
-									oscMsg.addArgument(smsMessageText);
-									mOscOut.send(oscMsg);
-								}
-								catch(IOException e){}
-								catch(NullPointerException e){}
-						    }
-						});
-						thread.start();
+						// add to queue
+						int pan = (new Random()).nextInt(255);
+						int tilt = (new Random()).nextInt(255);
+						Log.d(TAG, "Adding: ("+smsMessageText+","+pan+","+tilt+") to queue");
+						msgQueue.offer(new MotorMessage(smsMessageText, pan, tilt));
+						checkQueues();
 					}
 				}
 			}
@@ -166,37 +121,6 @@ public class VoxPopuliActivity extends Activity implements TextToSpeech.OnInitLi
 		msgQueue = (msgQueue == null)?(new LinkedList<MotorMessage>()):msgQueue;
 		registerReceiver(mSMS, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
 
-		// OSC
-		try{
-			mOscIn = (mOscIn == null)?(new OSCPortIn(OSC_IN_PORT)):mOscIn;
-			mOscIn.addListener("/ffqmevox", mOscListener);
-			mOscIn.startListening();
-		}
-		catch(SocketException e){
-			Log.e(TAG, "socket exception in onCreate");
-		}
-
-		Thread thread = new Thread(new Runnable(){
-		    @Override
-		    public void run() {
-		    	try {
-		    		InetAddress ina = InetAddress.getByName(VOX_SERVER_ADDRESS);
-		    		mOscOut = (mOscOut == null)?(new OSCPortOut(ina,OSC_OUT_PORT)):mOscOut;
-		    		oscOutAdressString = ina.toString();
-		    	}
-		    	catch(SocketException e){}
-		    	catch(UnknownHostException e){}
-		    }
-		});
-		thread.start();
-		try{
-			thread.join();
-		}
-		catch(InterruptedException e){}
-		finally{
-			Log.d(TAG, "server address " + oscOutAdressString);
-		}
-
 		checkQueues();
 
 		// GUI
@@ -216,21 +140,15 @@ public class VoxPopuliActivity extends Activity implements TextToSpeech.OnInitLi
 		}
 	}
 
- 
 	@Override
 	public void onPause() {
 		super.onPause();
 	}
- 
+
 	@Override
 	public void onDestroy() {
 		unregisterReceiver(mSMS);
 		if(mTTS != null) mTTS.shutdown();
-		if(mOscIn != null) {
-			mOscIn.stopListening();
-			mOscIn.close();
-		}
-		if(mOscOut != null) mOscOut.close();
 		try{
 			if(mInputStream != null) mInputStream.close();
 			if(mOutputStream != null) mOutputStream.close();
@@ -370,30 +288,6 @@ public class VoxPopuliActivity extends Activity implements TextToSpeech.OnInitLi
 
 	public void quitActivity(View v){
 		finish();
-	}
-
-	public void testSystem(View v){
-		Thread thread = new Thread(new Runnable(){
-			@Override
-			public void run() {
-				try{
-					Log.d(TAG, "send message");
-					OSCMessage oscPingMsg = new OSCMessage("/ffqmeping");
-					oscPingMsg.addArgument(Integer.toString(OSC_IN_PORT));
-					mOscOut.send(oscPingMsg);
-					OSCMessage oscSmsMsg = new OSCMessage("/ffqmesms");
-					oscSmsMsg.addArgument("testando sistema fofoque me. sim? ou não?");
-					mOscOut.send(oscSmsMsg);
-				}
-				catch(IOException e){}
-				catch(NullPointerException e){}
-			}
-		});
-		thread.start();
-		try{
-			thread.join();
-		}
-		catch(InterruptedException e) {}
 	}
 
 	public void blinkLED(View v){
